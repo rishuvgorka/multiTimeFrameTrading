7cc2a374e28de2fb09acb782cd265d251431c5bb
rishuvgorka <rishuvgorka.rg@gmail.com>
2025-07-09 13:31:02 +0530

    binance client setup

---
+++ config.py
@@ -0,0 +1,4 @@
+API_KEY = 'UZnfcz1VSXtWj9Joa1upgad1Rr97tyMCQDct6zWR1vMXFCjZ9dcmo4TIZSHGtZDV'
+API_SECRET = 'GtljIGgihhtS6keVIPu26pKxnhSskFIl8LsRF31csTiWlVOscFwJngmudtoCduZf'
+BASE_URL = 'https://testnet.binance.vision/api'
+
